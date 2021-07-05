e65f953f9221dc13908c67a28a1bc8e356bd417e
Rohan Poojary <rohanpoojary10@gmail.com>
2021-07-05 19:48:37 -0400

    Update experience section to use dynamic images via graphQL

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,141 +1,139 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { graphql, StaticQuery } from "gatsby";
 import {
   companyImg,
   row,
   companyName,
   jobTitle,
   companyImgCol,
+  link,
 } from "./Experience.module.css";
 import Header from "./SectionHeader";
 
-const Experience = () => {
+const jobList = [
+  {
+    company: "Wattpad",
+    jobTitle: "Associate Engineer",
+    duration: "May 2021 - Present",
+    imgName: "wattpad",
+    desc: [
+      "Develop a fully responsive web application using React at massive scale (more than 2 billion daily requests and 90+ million users).",
+      "Collaborate with product, design and senior engineers to build new experiences for users.",
+    ],
+  },
+  {
+    company: "ICUBE UTM",
+    jobTitle: "Software Developer",
+    duration: "Sept 2020 - Feb 2021",
+    imgName: "icube",
+    desc: [
+      "Develop responsive websites for clients using HTML, CSS, JavaScript and MySQL.",
+      "Create wordpress plugins using PHP and JavaScript to speed up project implementation.",
+      "Implemented websites by following accessibility and Search Engine Optimization (SEO) guidelines.",
+    ],
+  },
+  {
+    company: "Royal Bank of Canada",
+    jobTitle: "Quality Engineer Intern",
+    duration: "May 2020 - Aug 2020",
+    imgName: "rbc",
+    desc: [
+      "Worked on the Client Cards team to automate UI Testing, authoring 350+ test cases, highest by an intern in the team.",
+      "Developed automation scripts in Python with Selenium, increasing automated test coverage by 48%.",
+      "Developed understandable, reliable and maintainable test suites in collaboration with the QA team and executed tests using CI/CD Tools like Jenkins.",
+    ],
+  },
+];
+
+const MyExperience = ({ data }) => {
   return (
     <div id="experience">
       <Header sectionName={`<experience />`} />
-      <Row className={`row ${row}`}>
-        <Col
-          className="align-self-center"
-          xs={{ span: 12, order: "last" }}
-          sm={10}
-          md={{ order: "first" }}
-        >
-          <div className={companyName}>Wattpad</div>
-          <div className={jobTitle}>
-            Associate Engineer | May 2021 - Present
-          </div>
-          <ul>
-            <li>
-              Develop responsive websites for clients using HTML, CSS, PHP and
-              MySQL.
-            </li>
-            <li>
-              Create wordpress plugins using PHP and JavaScript to speed up
-              project implementation.
-            </li>
-            <li>
-              Implemented website by following technical Search Engine
-              Optimization (SEO) guidelines.
-            </li>
-          </ul>
-        </Col>
-        <Col
-          className={`col align-self-center ${companyImgCol}`}
-          xs={{ span: 12, order: "first" }}
-          md={{ order: "last" }}
-        >
-          <StaticImage
-            alt="Icube utm"
-            src="../images/wattpad.png"
-            imgClassName={companyImg}
-            style={{"maxWidth": "500px"}}
-          />
-        </Col>
-      </Row>
-      <Row className={`row ${row}`}>
-        <Col
-          className="align-self-center"
-          xs={{ span: 12, order: "last" }}
-          sm={10}
-          md={{ order: "first" }}
-        >
-          <div className={companyName}>ICUBE UTM</div>
-          <div className={jobTitle}>
-            Software Developer | Sept 2020 - Feb 2021
-          </div>
-          <ul>
-            <li>
-              Develop responsive websites for clients using HTML, CSS, PHP and
-              MySQL.
-            </li>
-            <li>
-              Create wordpress plugins using PHP and JavaScript to speed up
-              project implementation.
-            </li>
-            <li>
-              Implemented website by following technical Search Engine
-              Optimization (SEO) guidelines.
-            </li>
-          </ul>
-        </Col>
-        <Col
-          className={`col align-self-center ${companyImgCol}`}
-          xs={{ span: 12, order: "first" }}
-          md={{ order: "last" }}
-        >
-          <StaticImage
-            alt="Icube utm"
-            src="../images/icube.png"
-            imgClassName={companyImg}
-            style={{"maxWidth": "500px"}}
-          />
-        </Col>
-      </Row>
-      <Row className={`row ${row}`}>
-        <Col
-          className="align-self-center"
-          xs={{ span: 12, order: "last" }}
-          sm={10}
-          md={{ order: "first" }}
-        >
-          <div className={companyName}>Royal Bank of Canada (RBC)</div>
-          <div className={jobTitle}>
-            Quality Engineer Intern | May 2020 - Aug 2020
-          </div>
-          <ul>
-            <li>
-              Worked on the Client Cards team to automate UI Testing, authoring
-              350+ test cases, highest by an intern in the team.
-            </li>
-            <li>
-              Developed automation scripts in Python with Selenium, increasing
-              automated test coverage by 48%.
-            </li>
-            <li>
-              Developed understandable, reliable and maintainable test suites in
-              collaboration with the QA team and executed tests using CI/CD
-              Tools like Jenkins.
-            </li>
-            <li>Check out my recommendation here!</li>
-          </ul>
-        </Col>
-        <Col
-          className={`col align-self-center ${companyImgCol}`}
-          xs={{ span: 12, order: "first" }}
-          md={{ order: "last" }}
-        >
-          <StaticImage
-            alt="Icube utm"
-            src="../images/rbc.png"
-            imgClassName={companyImg}
-            style={{"maxWidth": "500px"}}
-          />
-        </Col>
-      </Row>
+      {jobList.map((job) => {
+        let image = "";
+
+        if (job.imgName === "wattpad") {
+          image = getImage(data.wattpad.childImageSharp);
+        } else if (job.imgName === "rbc") {
+          image = getImage(data.rbc.childImageSharp);
+        } else {
+          image = getImage(data.icube.childImageSharp);
+        }
+
+        return (
+          <Row className={`row ${row}`}>
+            <Col
+              className="align-self-center"
+              xs={{ span: 12, order: "last" }}
+              sm={10}
+              md={{ order: "first" }}
+            >
+              <div className={companyName}>{job.company}</div>
+              <div className={jobTitle}>
+                {job.jobTitle} | {job.duration}
+              </div>
+              <ul>
+                {job.desc.map((bullet) => (
+                  <li>{bullet}</li>
+                ))}
+              </ul>
+            </Col>
+            <Col
+              className={`col align-self-center ${companyImgCol}`}
+              xs={{ span: 12, order: "first" }}
+              md={{ order: "last" }}
+            >
+              <GatsbyImage
+                image={image}
+                alt={job.imgName}
+                imgClassName={companyImg}
+              />
+            </Col>
+          </Row>
+        );
+      })}
     </div>
   );
 };
 
-export default Experience;
+export default function Experience(props) {
+  return (
+    <StaticQuery
+      query={graphql`
+        query {
+          wattpad: file(relativePath: { eq: "wattpad.png" }) {
+            childImageSharp {
+              gatsbyImageData(
+                width: 500
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+          rbc: file(relativePath: { eq: "rbc.png" }) {
+            childImageSharp {
+              gatsbyImageData(
+                width: 500
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+          icube: file(relativePath: { eq: "icube.png" }) {
+            childImageSharp {
+              gatsbyImageData(
+                width: 500
+                placeholder: BLURRED
+                formats: [AUTO, WEBP, AVIF]
+              )
+            }
+          }
+        }
+      `}
+      render={(data) => <MyExperience data={data} {...props} />}
+    />
+  );
+}
