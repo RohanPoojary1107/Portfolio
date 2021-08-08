74792095710da636315024383a008d014b4dc27c
Rohan Poojary <rohanpoojary10@gmail.com>
2021-08-08 14:50:39 -0400

    add skip link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -38,73 +38,76 @@ const NavBar = ({ theme, toggleTheme }) => {
   const resume = data.allFile.edges[0];
 
   return (
-    <Navbar
-      collapseOnSelect
-      expand="sm"
-      variant={theme}
-      className={`navbar ${nav}`}
-    >
-      <Navbar.Brand as={Link} to="/">
-        <img
-          alt="Rohan Poojary logo"
-          height="25px"
-          width="125px"
-          src={lightMode ? rohanLight : rohanDark}
-          className={logo}
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Link
-            as={Link}
-            to="#about"
-            style={{ color: lightMode ? "black" : "white" }}
-          >
-            About
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="#experience"
-            style={{ color: lightMode ? "black" : "white" }}
-          >
-            Experience
-          </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="#projects"
-            style={{ color: lightMode ? "black" : "white" }}
-          >
-            Projects
-          </Nav.Link>
-          <Nav.Link
-            href={resume.node.publicURL}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="font-weight-medium"
-            style={{ color: lightMode ? "black" : "white" }}
-          >
-            Resume
-          </Nav.Link>
-          <button className={changeMode} onClick={() => toggleTheme()}>
-            <div className={imgWrapper}>
-              <img
-                alt={`toggle ${theme} mode`}
-                src={theme === "light" ? moon : sun}
-                className={modeIcon}
-              />
-              {!lightMode && <img className={sunRings} src={sunRing} alt="" />}
-              {lightMode && (
-                <>
-                  <img className={starTop} src={topStar} alt="" />
-                  <img className={starBottom} src={bottomStar} alt="" />
-                </>
-              )}
-            </div>
-          </button>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <>
+      <a href="#about" className="skip-link">Skip to content</a>
+      <Navbar
+        collapseOnSelect
+        expand="sm"
+        variant={theme}
+        className={`navbar ${nav}`}
+      >
+        <Navbar.Brand as={Link} to="/">
+          <img
+            alt="Rohan Poojary logo"
+            height="25px"
+            width="125px"
+            src={lightMode ? rohanLight : rohanDark}
+            className={logo}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link
+              as={Link}
+              to="#about"
+              style={{ color: lightMode ? "black" : "white" }}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="#experience"
+              style={{ color: lightMode ? "black" : "white" }}
+            >
+              Experience
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="#projects"
+              style={{ color: lightMode ? "black" : "white" }}
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              href={resume.node.publicURL}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-weight-medium"
+              style={{ color: lightMode ? "black" : "white" }}
+            >
+              Resume
+            </Nav.Link>
+            <button className={changeMode} onClick={() => toggleTheme()}>
+              <div className={imgWrapper}>
+                <img
+                  alt={`toggle ${theme} mode`}
+                  src={theme === "light" ? moon : sun}
+                  className={modeIcon}
+                />
+                {!lightMode && <img className={sunRings} src={sunRing} alt="" />}
+                {lightMode && (
+                  <>
+                    <img className={starTop} src={topStar} alt="" />
+                    <img className={starBottom} src={bottomStar} alt="" />
+                  </>
+                )}
+              </div>
+            </button>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 };
 
