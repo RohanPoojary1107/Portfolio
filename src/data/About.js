c36d75b695c6d59b0764a3199cf9f38c52f36bd7
Rohan Poojary <rohanpoojary10@gmail.com>
2022-03-28 20:25:03 -0400

    Add data folder and move static data in it.

---
+++ src/data/About.js
@@ -0,0 +1,19 @@
+const ABOUT = `My Mantra: “You have to keep working on your craft, specially when
+    nobody is watching” <br />
+    <br />
+    I have been working with Python and Java for two years and am highly
+    experienced in working in an Agile environment. My favorite side
+    project so far has to be developing an anonymous hiring app using
+    Flutter with the goal of mitigating discrimination in the hiring
+    process for companies. <br />
+    <br />
+    I’m always open to taking on exciting challenges and developing new
+    skills in the process. Outside my coursework, I am currently learning
+    to develop web applications using Javascript and delve deeper into app
+    development with Flutter. <br />
+    <br />
+    If you feel like you may have an opportunity for me or would just love
+    to chat about anything from basketball to technology, feel free to
+    connect with me on LinkedIn or email me!`;
+
+export default ABOUT;
---
+++ src/data/Intro.js
@@ -0,0 +1,7 @@
+const INTRO = {
+  title: "Hey, I'm Rohan.",
+  subTitle:
+    "I'm passionate about making the web more accessible and I enjoy pushing myself outside my comfort zone.",
+};
+
+export default INTRO;
