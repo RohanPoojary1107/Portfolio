64c66d5c9084776121572451fa7eb8e539239065
Rohan Poojary <rohanpoojary10@gmail.com>
2022-06-26 17:07:31 -0400

    Migrate pages directory to typescript

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "../components/App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fontsource/inter";
+
+const notFound = () => {
+  return <App isInvalid />;
+};
+
+export default notFound;
