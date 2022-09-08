758beb6fec1b7ecf67355db861770a464f7a90a3
Rohan Poojary <rohanpoojary10@gmail.com>
2022-09-08 19:03:04 -0400

    Update types

---
+++ src/global-types.ts
@@ -0,0 +1,14 @@
+export enum CompanyCode {
+  RBC = "rbc",
+  ICUBE = "icube",
+  WATTPAD = "wattpad",
+  UOFT = "uoft"
+}
+
+export type Project = {
+  name: string;
+  techStack: string[];
+  desc: string;
+  link: string;
+  isPrivate: boolean;
+};
