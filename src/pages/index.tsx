83dd7be96e64f8f23597c9fd2b9bbbc70994a027
Rohan Poojary <rohanpoojary10@gmail.com>
2023-03-13 23:08:38 -0400

    Remove snow effect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Script } from "gatsby";
 import App from "../components/App";
 import SEO from "../components/Seo";
 
@@ -7,12 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "@fontsource/inter";
 
 const Home = () => {
-  return (
-    <>
-      <Script src="https://app.embed.im/snow.js" />
-      <App />;
-    </>
-  );
+  return <App />;
 };
 
 export default Home;
